0e9aba6e97478d240075007ce058c12149dd2ae5
ItalianStyle <60391359+ItalianStyle@users.noreply.github.com>
2021-07-03 16:51:49 +0200

    Imported 2 more scripts from Larochien's Adventure
    - Imported ObjectPooler (IMPORTANT)
    - Imported PauseListener

---
+++ ZombieAttack/Assets/Scripts/PauseListener.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Larochiens_Adventure
+{
+    public class PauseListener : MonoBehaviour
+    {
+        [SerializeField] bool pauseState = false;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                pauseState = !pauseState;
+                GameManager.instance.SetStatusGame(GameManager.GameState.Paused);
+                UI_Manager.instance.SetFinishScreen(GameManager.GameState.Paused);
+            }
+        }
+    }
+}
