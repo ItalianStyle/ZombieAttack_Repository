e27bc263e040497cfd334a95904359e6ba83777e
ItalianStyle <60391359+ItalianStyle@users.noreply.github.com>
2021-07-03 19:07:38 +0200

    Update Bullet.cs
    - Added event logic when bullet hits enemy
    - Added Throw() to add damage togheter with force to bullet Rigidbody

--- ZombieAttack/Assets/Scripts/Bullet.cs
+++ ZombieAttack/Assets/Scripts/Bullet.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public event Action<float> OnEnemyHit;
+
+    Rigidbody bulletRigidbody;
+    float damage;
+
+    private void Awake()
     {
-        
+        bulletRigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            OnEnemyHit?.Invoke(damage);
+        }
+        gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        transform.rotation = Quaternion.identity;
+        transform.position = Vector3.zero;
+        bulletRigidbody.velocity = Vector3.zero;
+    }
+
+    public void Throw(Vector3 force, ForceMode forceMode, float damage)
+    {
+        bulletRigidbody.AddForce(force, forceMode);
+        this.damage = damage;
     }
 }
