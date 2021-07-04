80d9c576cce6384a641f8dcfe4f73fca6baf75fd
ItalianStyle <60391359+ItalianStyle@users.noreply.github.com>
2021-07-04 16:45:26 +0200

    Reworked Health component to match HealthBar requisites
    - Introduced maxHealth variable to match SimpleHealthBar requisites
    
    - Added event logic when health changes, then HealthBar updates
    
    - Introduced ModifyHealth method (possible future uses as healing mechanic)

--- ZombieAttack/Assets/Scripts/Health.cs
+++ ZombieAttack/Assets/Scripts/Health.cs
@@ -1,27 +1,46 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace ZombieAttack
 {
     public class Health : MonoBehaviour
     {
-        [SerializeField] float health = 100;
+        [SerializeField] float maxHealth = 100;
+        float currentHealth;
 
-        public void DealDamage(float damage)
+        public event Action<float, float> OnHealthPctChanged = delegate { }; //delegate is to avoid null checks
+
+        private void OnEnable() => currentHealth = maxHealth;
+
+        void ModifyHealth(float amount)
         {
-            if (health > 0f)
+            if (currentHealth > 0f && currentHealth <= maxHealth)
             {
-                health -= damage;
-                if (health <= 0f)
+                currentHealth += amount;
+                if (currentHealth <= 0f)
                 {
                     gameObject.SetActive(false);
-                    if (gameObject.CompareTag("Finish"))
+
+                    if (gameObject.CompareTag("Finish"))  //Check if it's FinalObjective
                     {
-                        //Game Over
-                        Debug.Log("GAME OVER");
+                        Debug.Log("GAME OVER");  //Game Over
                     }
-                        
+                    else if (gameObject.CompareTag("Player"))
+                    {
+                        Debug.Log("Sei morto");
+                    }
+                    else if (gameObject.CompareTag("Enemy"))
+                    {
+                        //Play sound
+                        //Earn money
+                    }
                 }
+                else if (currentHealth > maxHealth)
+                    currentHealth = maxHealth;
+                OnHealthPctChanged(currentHealth, maxHealth);
             }
         }
+
+        public void DealDamage(float damage) => ModifyHealth(-damage);
     }
 }
