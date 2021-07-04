72fd7f4233213e3fd50269ebceca2f92b8be1b49
ItalianStyle <60391359+ItalianStyle@users.noreply.github.com>
2021-07-04 16:30:41 +0200

    Added HealthBar script

---
+++ ZombieAttack/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZombieAttack
+{
+    public class HealthBar : MonoBehaviour
+    {
+        public SimpleHealthBar enemyHealthBar;
+
+        Camera mainCamera;
+
+        private void Awake()
+        {
+            mainCamera = Camera.main;
+            GetComponentInParent<Health>().OnHealthPctChanged += HandleHealthChanged;
+        }
+
+        private void HandleHealthChanged(float currentHealth, float maxHealth)
+        {
+            enemyHealthBar.UpdateBar(currentHealth, maxHealth);
+        }
+
+        private void LateUpdate()
+        {
+            transform.LookAt(mainCamera.transform);
+            transform.Rotate(0, 180, 0);
+        }
+    }
+}
