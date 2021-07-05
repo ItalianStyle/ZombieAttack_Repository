316fa4767249073fad0cf6339ad6b208f10ad9b0
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2021-07-05 12:53:20 +0200

    Added FinishScreen script

---
+++ ZombieAttack/Assets/Scripts/FinishScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Finish Screen", menuName = "Finish Screen Type")]
+public class FinishScreen : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
