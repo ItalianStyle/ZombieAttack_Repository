6af65e39548203f50b42b7f7c2bd266d0753aad4
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2021-07-05 20:09:21 +0200

    Debug
    - Created KillCounter component to show kills of the player
    - The enemy is weaker than before to win faster

---
+++ ZombieAttack/Assets/KillCounter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ZombieAttack
+{
+    public class KillCounter : MonoBehaviour
+    {
+        [SerializeField] EnemyManager enemyManager;
+
+        private void Update()
+        {
+            GetComponent<Text>().text = enemyManager.killedEnemies.ToString();
+        }
+    }
+}
