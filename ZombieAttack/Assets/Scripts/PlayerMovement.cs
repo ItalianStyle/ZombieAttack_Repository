3a414826b2e531fc98e8f09f86ca9fdc6b655495
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2021-07-06 12:53:04 +0200

    Update PlayerMovement.cs
    - Cleaned commented code
    
    - Changed CharacterController moving method from Move() to SimpleMove() to avoid strange behaviours
    
    - No more time.deltatime factor in SimpleMove() calculation to avoid complex and unrealistic values in movSpeed

--- ZombieAttack/Assets/Scripts/PlayerMovement.cs
+++ ZombieAttack/Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,6 @@ namespace ZombieAttack
 {
     public class PlayerMovement : MonoBehaviour
     {
-        /*[Tooltip("How fast player can move?")]
-        [SerializeField] float movementSpeed = 10f;
-        [Tooltip("How fast player can rotate around himself?")]
-        [SerializeField] float rotationSpeed = 30f;
-        CharacterController playerController;
-        */
         [SerializeField] CharacterController Controller;
         Vector3 input;
         public float movSpeed;
@@ -23,15 +17,9 @@ namespace ZombieAttack
 
         private void Update()
         {
-            /*//Apply movement
-            playerController.SimpleMove(Input.GetAxis("Vertical") * movementSpeed * transform.forward);
-            //Apply rotation
-            transform.Rotate(Input.GetAxis("Horizontal") * rotationSpeed * transform.up);
-            */
-
             input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
             Vector3 moveDir = Quaternion.Euler(0, cam.transform.eulerAngles.y, 0) * input;
-            Controller.Move(moveDir.normalized * movSpeed * Time.deltaTime);
+            Controller.SimpleMove(moveDir.normalized * movSpeed);
 
             if (input.magnitude > 0)
             {
