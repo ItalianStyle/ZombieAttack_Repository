436453587ef7302a6e09ecaa63778222d7989936
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2021-07-07 10:23:13 +0200

    Improved SpawnEnemy mechanic
    - Moved SpawnEnemy() and all related methods to Wave class
    
    - Added Waves array and SpawnNextWave()

--- ZombieAttack/Assets/Scripts/EnemyManager.cs
+++ ZombieAttack/Assets/Scripts/EnemyManager.cs
@@ -12,9 +12,9 @@ namespace ZombieAttack
         [SerializeField] Transform finalObjectiveTransform = null;
         [SerializeField] float timeBetweenSpawns = 1f;
         [SerializeField] int maxEnemies = 10;
+        [SerializeField] Wave[] waves;
+        int currentWave = 0;
 
-        int currentEnemies = 0;
-        public int killedEnemies = 0;
         ObjectPooler objPooler;
 
         //public static EnemySpawnManager instance;
@@ -37,43 +37,18 @@ namespace ZombieAttack
             for(int i = 0; i < transform.childCount; i++)
                 spawnPoints.Add(transform.GetChild(i));
             
-            currentEnemies = 0;
-            SpawnEnemy();
+            currentWave = 0;
+            SpawnNextWave(); 
         }
 
-        void SpawnEnemy()
+        void SpawnNextWave()
         {
-            GameObject enemy = objPooler.GetPooledObject("Enemy");
-            
-            //Choose spawnpoint
-            enemy.transform.position = spawnPoints[Random.Range(0, spawnPoints.Count)].position;
-
-            enemy.GetComponent<EnemyMovement>().SetDestination(finalObjectiveTransform);
-            enemy.GetComponent<Health>().OnEnemyDead += IncreaseKillCount;
-            enemy.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            enemy.SetActive(true);
-
-            currentEnemies++;
-            if(currentEnemies < maxEnemies)
-                StartCoroutine(WaitBeforeSpawn());
-        }
-
-        private void IncreaseKillCount(Health enemyHealth)
-        {
-            killedEnemies++;
-            enemyHealth.OnEnemyDead -= IncreaseKillCount;
-            if(killedEnemies >= maxEnemies)
+            if(currentWave > 0 && currentWave < waves.Length)
+                waves[currentWave++].SpawnEnemy(objPooler, spawnPoints, finalObjectiveTransform);
+            else
             {
-                //Vittoria
-                GameManager.instance.SetStatusGame(GameManager.GameState.Won);
-                UI_Manager.instance.SetFinishScreen(GameManager.GameState.Won);
+                Debug.Log("Current wave fuori dal range: " + currentWave);
             }
-        }
-
-        IEnumerator WaitBeforeSpawn()
-        {
-            yield return new WaitForSeconds(timeBetweenSpawns);
-            SpawnEnemy();
         }
     }
 }
