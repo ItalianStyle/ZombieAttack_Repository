2fa6d321d0deb23e012da66168525a83838e894a
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2021-07-11 11:41:50 +0200

    Created WaveBeginnerFlag

---
+++ ZombieAttack/Assets/Scripts/WaveBeginnerFlag.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace ZombieAttack
+{
+    //Questa classe è usata dal waveText per cominciare l'ondata di nemici dopo la scritta
+    public class WaveBeginnerFlag : MonoBehaviour
+    {
+        [SerializeField] int timeToStartWave = 1;
+
+        //Called in the end of WaveText_appearing animation
+        public void StartEnemyWave() => EnemyManager.instance.SpawnWave();
+
+        public void StartTimer()
+        {
+            UI_Manager.instance.SetTimerText(true);
+            StartCoroutine(WaitForNextWave());
+        }
+
+        IEnumerator WaitForNextWave()
+        {
+            for(int timer = timeToStartWave; timer > 0;)
+            {
+                UI_Manager.instance.UpdateTimeText(timer);
+                yield return new WaitForSeconds(1);
+                timer--;
+            }
+            //Tolgo il timer
+            UI_Manager.instance.SetTimerText(false);
+            //Faccio partire l'animazione del testo di presentazione ondata
+            UI_Manager.instance.PlayWaveText(isVictoryText: false);
+        }
+    }
+}
