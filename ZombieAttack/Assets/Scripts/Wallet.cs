5caf5f93a81fdc4de2088203f758fa6cdc3d5c63
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2021-07-12 11:05:02 +0200

    Created Wallet.cs and added UpdateCurrentMoney()

---
+++ ZombieAttack/Assets/Scripts/Wallet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZombieAttack
+{
+    public class Wallet : MonoBehaviour
+    {
+        [SerializeField] int currentMoney = 0;
+
+        public void UpdateCurrentMoney(int amount, bool isDetraction)
+        {
+            if (isDetraction)
+            {
+                currentMoney -= amount;
+                if (currentMoney < 0)
+                    currentMoney = 0;
+            }
+            else
+                currentMoney += amount;
+        }
+    }
+}
