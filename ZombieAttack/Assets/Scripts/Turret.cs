874b14cb8384e94dd948335014d218bfc9aefebb
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2021-07-18 11:34:27 +0200

    Update Turret.cs
    - a turret has a activation bool
    - if an enemy enters in range, the turret will look at enemy
    - if multiple enemies enters in range, turret will look at 1st entered enemy and changes to the 2nd entered when the 1st one left the range

--- ZombieAttack/Assets/Scripts/Turret.cs
+++ ZombieAttack/Assets/Scripts/Turret.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Turret : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace ZombieAttack
+{ 
+    public class Turret : MonoBehaviour
     {
-        
-    }
+        public bool isActive = false;
+        List<EnemyMovement> enemiesOnSight;
 
-    // Update is called once per frame
-    void Update()
-    {
+        private void Start()
+        {
+            enemiesOnSight = new List<EnemyMovement>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (isActive && other.TryGetComponent(out EnemyMovement enemy))
+            {
+                if (!enemiesOnSight.Contains(enemy))             
+                    enemiesOnSight.Add(enemy);              
+            }
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (isActive && enemiesOnSight.Count > 0)
+                transform.LookAt(enemiesOnSight[0].transform);
+                    
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (isActive && other.TryGetComponent(out EnemyMovement enemy))
+            {
+                if (enemiesOnSight.Contains(enemy))
+                {
+                    enemiesOnSight.Remove(enemy);
+                    if(enemiesOnSight.Count == 0)
+                        transform.localRotation = Quaternion.identity;
+                }
+            }
+        }
     }
 }
