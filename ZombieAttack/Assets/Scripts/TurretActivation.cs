83cf6f62369f2561cf8b28b7854d0f196b9c9a59
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2021-07-19 19:50:54 +0200

    Created TurretActivation component
    Each turret needs to be activated manually by the player if he is in range
    - When player is near, a panel appears to say what key to press
    - If player leaves the area without pressing the key, the panel will disappear without consequences
    -If player presses the key the turret is instantly activated without money cost yet.

---
+++ ZombieAttack/Assets/Scripts/TurretActivation.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace ZombieAttack
+{
+    public class TurretActivation : MonoBehaviour
+    {
+        Turret turret;
+        [SerializeField] Transform pivotText = null;
+        [SerializeField] CanvasGroup activateTurretPanel = null;
+        Camera cam;
+
+        private void Awake()
+        {
+            turret = GetComponentInChildren<Turret>();
+            pivotText = transform.GetChild(0);
+            activateTurretPanel = GameObject.Find("UI/ActivateTurretPanel").GetComponent<CanvasGroup>();
+            cam = Camera.main;
+        }
+
+        private void Start()
+        {
+            UI_Manager.instance.SetCanvasGroup(activateTurretPanel, false);
+            turret.enabled = false;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player") && !turret.enabled)
+            {
+                //Mostra il testo
+                UI_Manager.instance.SetCanvasGroup(activateTurretPanel, true);
+            }
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if(other.CompareTag("Player") && !turret.enabled)
+            {
+                //activateTurretPanel.transform.position = cam.WorldToScreenPoint(pivotText.position);
+                if (Input.GetKey(KeyCode.E))
+                {
+                    UI_Manager.instance.SetCanvasGroup(activateTurretPanel, false);
+                    turret.enabled = true;
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if(other.CompareTag("Player"))
+            {
+                UI_Manager.instance.SetCanvasGroup(activateTurretPanel, false);
+            }
+        }
+    }
+}
