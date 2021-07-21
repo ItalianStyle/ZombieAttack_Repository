f9eec44c802bd1d8844a7cb43dbeef0d44aaccba
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2021-07-21 20:02:53 +0200

    Update Gun.cs
    - Introduced GunType enum to distinguish various guns for player: Rifle and Shotgun
    
    - Imported Shotgun variables and methods from a UnityForum script (at the moment is uselss, using ParticleSystem instead)

--- ZombieAttack/Assets/Scripts/Gun.cs
+++ ZombieAttack/Assets/Scripts/Gun.cs
@@ -6,20 +6,41 @@ namespace ZombieAttack
 {
     public class Gun : MonoBehaviour
     {
-        ObjectPooler bulletMagazine;
+        public enum GunType { Rifle, Shotgun }
+        public GunType gunType;
+
         Transform muzzleTransform;
+        ShotgunBulletHandler shotgunBullets;
+
         Transform playerTransform;
+        ObjectPooler bulletMagazine;
+        
+        [Header("Rifle variables")]
         [SerializeField] float reloadTime = .2f;
         [SerializeField] float shootForce = 10f;
         [SerializeField] float bulletLifetime = 1f;
         [SerializeField] float damage = 1f;
+
+        [Header("Shotgun variables")]
+        //These 2 controls the spread of the cone
+        public float scaleLimit = 2.0f;
+        public float z = 10f;
+        //Shoots multiple rays to check the programming
+        public int count = 30;
         bool canShoot = true;
 
         private void Awake()
         {
             bulletMagazine = GetComponent<ObjectPooler>();
             muzzleTransform = transform.Find("Muzzle");
+            shotgunBullets = muzzleTransform.GetComponentInChildren<ShotgunBulletHandler>();
             playerTransform = transform.parent;
+        }
+
+        private void Start()
+        {
+            if (gunType is GunType.Shotgun)
+                shotgunBullets.BulletDamage = damage;
         }
 
         public void Shoot(SimpleHealthBar gunBar)
@@ -27,20 +48,67 @@ namespace ZombieAttack
             if (canShoot)
             {
                 playerTransform.GetComponent<PlayerMovement>().FaceCamera();
-                                
-                //Posiziono il bullet
-                GameObject bullet = bulletMagazine.GetPooledObject("Bullet");
-                bullet.transform.position = muzzleTransform.position;
-                bullet.transform.forward = muzzleTransform.forward;
-                
-                //Shooting
-                bullet.SetActive(true);
-                bullet.GetComponent<Bullet>().Throw(bullet.transform.forward * shootForce, ForceMode.Impulse, damage);
+                switch(gunType)
+                {
+                    case GunType.Rifle:
+                        //Posiziono il bullet
+                        GameObject bullet = bulletMagazine.GetPooledObject("Bullet");
+                        bullet.transform.position = muzzleTransform.position;
+                        bullet.transform.forward = muzzleTransform.forward;
+
+                        //Shooting
+                        bullet.SetActive(true);
+                        bullet.GetComponent<Bullet>().Throw(bullet.transform.forward * shootForce, ForceMode.Impulse, damage);
+                        
+                        StartCoroutine("CountBulletLifetime", bullet);
+
+                        break;
+
+                    case GunType.Shotgun:
+                        /*for (int i = 0; i < count; ++i)
+                            ShootRay();   
+                        */
+                        shotgunBullets.Particles.Play();
+                        break;
+                }
+                shotgunBullets.TryGetComponent(out ParticleSystem ps);
+                ps.Play();
 
                 //Post-shoot stuff
                 StartCoroutine("Reload", gunBar);
-                StartCoroutine("CountBulletLifetime", bullet);
                 canShoot = false;
+            }
+            
+        }
+
+        void ShootRay()
+        {
+            //  The Ray-hits will be in a circular area
+            float randomRadius = Random.Range(0, scaleLimit);
+            float randomAngle = Random.Range(0, 2 * Mathf.PI);
+
+            //Calculating the raycast direction
+            Vector3 direction = new Vector3(
+                randomRadius * Mathf.Cos(randomAngle),
+                randomRadius * Mathf.Sin(randomAngle),
+                z
+            );
+
+            //Make the direction match the transform
+            //It is like converting the Vector3.forward to transform.forward
+            direction = muzzleTransform.TransformDirection(direction.normalized);
+
+            //Raycast and debug
+            Ray r = new Ray(muzzleTransform.position, direction);
+            RaycastHit hit;
+            if (Physics.Raycast(r, out hit))
+            {
+                //Debug.DrawLine(muzzleTransform.position, hit.point);
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    Debug.Log("Nemico colpito");
+                    hit.collider.GetComponent<Health>().DealDamage(damage / count);
+                }
             }
         }
 
