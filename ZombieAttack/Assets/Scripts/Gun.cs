93c3c72fb08bc03595990c32036e7dd97f8fb9b8
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2021-07-21 20:06:07 +0200

    Update Gun.cs
    Removed old code about Raycast approach for shooting with shotgun

--- ZombieAttack/Assets/Scripts/Gun.cs
+++ ZombieAttack/Assets/Scripts/Gun.cs
@@ -21,12 +21,6 @@ namespace ZombieAttack
         [SerializeField] float bulletLifetime = 1f;
         [SerializeField] float damage = 1f;
 
-        [Header("Shotgun variables")]
-        //These 2 controls the spread of the cone
-        public float scaleLimit = 2.0f;
-        public float z = 10f;
-        //Shoots multiple rays to check the programming
-        public int count = 30;
         bool canShoot = true;
 
         private void Awake()
@@ -79,37 +73,6 @@ namespace ZombieAttack
                 canShoot = false;
             }
             
-        }
-
-        void ShootRay()
-        {
-            //  The Ray-hits will be in a circular area
-            float randomRadius = Random.Range(0, scaleLimit);
-            float randomAngle = Random.Range(0, 2 * Mathf.PI);
-
-            //Calculating the raycast direction
-            Vector3 direction = new Vector3(
-                randomRadius * Mathf.Cos(randomAngle),
-                randomRadius * Mathf.Sin(randomAngle),
-                z
-            );
-
-            //Make the direction match the transform
-            //It is like converting the Vector3.forward to transform.forward
-            direction = muzzleTransform.TransformDirection(direction.normalized);
-
-            //Raycast and debug
-            Ray r = new Ray(muzzleTransform.position, direction);
-            RaycastHit hit;
-            if (Physics.Raycast(r, out hit))
-            {
-                //Debug.DrawLine(muzzleTransform.position, hit.point);
-                if (hit.collider.CompareTag("Enemy"))
-                {
-                    Debug.Log("Nemico colpito");
-                    hit.collider.GetComponent<Health>().DealDamage(damage / count);
-                }
-            }
         }
 
         IEnumerator Reload(SimpleHealthBar gunBar)
