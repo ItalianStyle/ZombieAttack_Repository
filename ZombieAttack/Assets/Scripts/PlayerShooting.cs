95b74e594ad6d3b9dee076518f23abeb98060ba4
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2021-07-22 12:42:26 +0200

    Update PlayerShooting.cs
    - Created Gun array structure _currentGun index
    - Created CurrentGun property to cycle among all guns in the array by disabling all of them except the used one
    - By scrolling the player can cycle through guns
    - Created SetupGuns() to inizialize the Gun array structure

--- ZombieAttack/Assets/Scripts/PlayerShooting.cs
+++ ZombieAttack/Assets/Scripts/PlayerShooting.cs
@@ -1,22 +1,70 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace ZombieAttack
 {
     public class PlayerShooting : MonoBehaviour
     {
-        Gun currentGun;
+        [SerializeField] Gun[] guns;
+        [SerializeField] int _currentGun;
+        [SerializeField] bool canScrollMouse = false;
         [SerializeField] SimpleHealthBar gunBar = null;
+         int CurrentGun
+        {
+            get => _currentGun;
+            set
+            {
+                guns[_currentGun].SetGunState(false);
 
+                _currentGun = value;                              
+                if (_currentGun < 0)
+                    _currentGun = guns.Length - 1;
+                else if (_currentGun >= guns.Length)
+                    _currentGun = 0;
+
+                guns[_currentGun].SetGunState(true);
+            }
+        }
         private void Awake()
         {
-            currentGun = transform.Find("Shotgun").GetComponent<Gun>();    
+            guns = GetComponentsInChildren<Gun>();
+        }
+
+        private void Start()
+        {
+            canScrollMouse = true;
+            CurrentGun = 0;
+            SetupGuns();
         }
 
         private void Update()
         {
             if (Input.GetMouseButton(0))
+                guns[CurrentGun].Shoot(gunBar);
+            
+            if (canScrollMouse)
             {
-                currentGun.Shoot(gunBar);
+                if (Input.GetAxis("Mouse ScrollWheel") > 0)
+                    CurrentGun++;
+                
+                if(Input.GetAxis("Mouse ScrollWheel") < 0)
+                    CurrentGun--;
+            }
+        }
+
+        void SetupGuns()
+        {
+            for(int i = 0; i < guns.Length; i++)
+            {
+                if (i != CurrentGun)
+                {
+                    guns[i].SetGunState(false);
+                }
+                else
+                {
+                    guns[i].SetGunState(true);
+                }
             }
         }
     }
