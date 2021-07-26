97e1687651207aeac5aafd4b7711e4af654eff4b
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2021-07-26 10:29:01 +0200

    Renamed ShotgunBulletHandler script to BulletHandler
    - Because Gun will use particle system to shoot bullets too

---
+++ ZombieAttack/Assets/Scripts/BulletHandler.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZombieAttack
+{
+    public class BulletHandler : MonoBehaviour
+    {
+        Gun.GunType bulletType;
+        float _bulletDamage;
+        int totalBullets;
+        ParticleSystem bulletsParticleSystem;
+        List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
+
+        [Min(0.01f)] float rateOfFire;
+        public float BulletDamage 
+        { 
+            private get => _bulletDamage; 
+            set 
+            {
+                switch (bulletType)
+                {
+                    case Gun.GunType.Rifle:
+                        _bulletDamage = value > 0 ? value : 1f;
+                        break;
+
+                    case Gun.GunType.Shotgun:
+                        _bulletDamage = value > 0 ? value / totalBullets : .1f;
+                        break;
+                }
+            }
+        }
+
+        public ParticleSystem Particles { get => bulletsParticleSystem; }
+
+        private void Awake()
+        {
+            bulletsParticleSystem = GetComponent<ParticleSystem>();
+        }
+
+        private void Start()
+        {
+            switch (bulletType)
+            {
+                case Gun.GunType.Rifle:
+
+                    break;
+
+                case Gun.GunType.Shotgun:
+                    totalBullets = (int)Particles.emission.GetBurst(0).count.constant;
+                    break;
+            }
+        }
+        private void OnParticleCollision(GameObject other)  //how to shoot with particle system https://www.youtube.com/watch?v=lkq8iLOr3sw&t=13s
+        {
+            int events = bulletsParticleSystem.GetCollisionEvents(other, collisionEvents);
+            for(int i = 0; i < events; i++)
+            {
+                //Spark particles
+            }
+            if (other.layer == LayerMask.NameToLayer("Enemy"))
+            {
+                Debug.Log("Colpito");
+                other.GetComponent<Health>().DealDamage(BulletDamage);
+            }
+        }
+    }
+}
