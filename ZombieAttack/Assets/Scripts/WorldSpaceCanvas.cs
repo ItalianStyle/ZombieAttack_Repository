c3e1a6f5b526b04155708416d103e11d87d055b5
ItalianStyle <60391359+ItalianStyle@users.noreply.github.com>
2021-07-30 19:38:15 +0200

    Created WorldSpaceCanvas component
    This component handles a UI canvas in world space for turrets and pickups, when player is near them, an event system will handle the appearance of UI and what is written according to what is near the player (Health, Shotgun, Turret) and in which situation is the object (Shotgun not bought, turret not activated etc.)
    
    - In Health pickup it will spawn a timer to show how much time is left before respawning
    
    - In Turrets and Shotgun the UI canvas is shown using events

---
+++ ZombieAttack/Assets/Scripts/WorldSpaceCanvas.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ZombieAttack
+{
+    public class WorldSpaceCanvas : MonoBehaviour
+    {
+        enum CanvasMode { None, Pickup, Turret }
+        public event Action OnTimerFinished = delegate { };
+
+        [SerializeField] CanvasMode worldSpaceCanvasMode = CanvasMode.None;
+        Camera mainCamera;
+        [SerializeField] Pickup pickup;
+        [SerializeField] TurretActivation turretActivation;
+
+        [SerializeField] CanvasGroup thisCanvasGroup;       
+        Text text; //Can be countdown text or shop text
+        Image image; //Can be timer background image (filled) or keyIcon
+
+        private void Awake()
+        {
+            mainCamera = Camera.main;
+            if (pickup = GetComponentInParent<Pickup>())
+                worldSpaceCanvasMode = CanvasMode.Pickup;
+            else if (turretActivation = GetComponentInParent<TurretActivation>())
+                worldSpaceCanvasMode = CanvasMode.Turret;
+            else
+                worldSpaceCanvasMode = CanvasMode.None;
+
+            thisCanvasGroup = GetComponent<CanvasGroup>();
+            text = GetComponentInChildren<Text>();           
+            image = GetComponentInChildren<Image>();                            
+        }
+
+        private void OnEnable()
+        {
+            UI_Manager.SetCanvasGroup(thisCanvasGroup, true);
+            switch (worldSpaceCanvasMode)
+            {
+                case CanvasMode.Pickup:
+                    switch (pickup.pickupType)
+                    {
+                        case Pickup.PickupType.Health:
+                            
+                            pickup.OnPickupTake -= EnableTimer;
+                            break;
+                        case Pickup.PickupType.Shotgun:
+                            pickup.OnPickupTake += () => enabled = false;
+                            pickup.OnPlayerFar += () => enabled = false;
+                            pickup.OnPlayerNear -= () =>
+                            {
+                                SetupPickupText();
+                                enabled = true;
+                            };
+
+                            break;
+                    }
+                    break;
+
+                case CanvasMode.Turret:
+                    turretActivation.OnPlayerOutOfRange += () => enabled = false;
+                    turretActivation.OnPlayerInRange -= SetupShopText;
+
+                    turretActivation.Turret.OnTurretEnabled += SetupShopText;
+                    turretActivation.Turret.OnTurretDisabled += SetupShopText;
+                    break;
+            }      
+        }        
+
+        private void Start()
+        {   
+            enabled = false;
+        }
+
+        void Update()
+        {
+            transform.LookAt(mainCamera.transform);
+            transform.Rotate(0, 180, 0);
+        }
+
+        private void OnDisable()
+        {
+            UI_Manager.SetCanvasGroup(thisCanvasGroup, false);
+            switch (worldSpaceCanvasMode)
+            {
+                case CanvasMode.Pickup:
+                    switch (pickup.pickupType)
+                    {
+                        case Pickup.PickupType.Health:
+                            pickup.OnPickupTake += EnableTimer;
+                            OnTimerFinished.Invoke();
+                            break;
+
+                        case Pickup.PickupType.Shotgun:
+                            pickup.OnPickupTake -= () => enabled = false;
+                            pickup.OnPlayerFar -= () => enabled = false;
+                            pickup.OnPlayerNear += () =>
+                            { 
+                                SetupPickupText(); 
+                                enabled = true;
+                            };
+                            break;
+                    }
+                    break;
+
+                case CanvasMode.Turret:
+                    turretActivation.OnPlayerOutOfRange -= () => enabled = false;
+                    turretActivation.OnPlayerInRange += SetupShopText;
+
+                    turretActivation.Turret.OnTurretEnabled -= SetupShopText;
+                    turretActivation.Turret.OnTurretDisabled -= SetupShopText;
+                    break;
+            }
+        }
+
+        private void SetupPickupText()
+        {
+            if (Wallet.instance.HasEnoughMoneyFor(pickup.gunToActivate.cost))
+                SetText(canActiveKeyIcon: true, Color.white, "Compra (-" + pickup.gunToActivate.cost.ToString() + "$)");
+
+            //Se il giocatore non ha i soldi
+            else
+                SetText(canActiveKeyIcon: false, Color.red, "Raccogli " + pickup.gunToActivate.cost.ToString() + "$");
+        }
+
+        private void SetupShopText(Turret turret)
+        {
+            //Se la torretta è già attiva
+            if (turret.enabled)
+                SetText(true, Color.white, "Disattiva (+" + turret.sellingCost.ToString() + "$)");
+
+            //Se il giocatore non ha attivato la torretta ed ha i soldi
+            else if (Wallet.instance.HasEnoughMoneyFor(turret.buildingCost))
+                SetText(true, Color.white, "Attiva (-" + turret.buildingCost.ToString() + "$)");
+
+            //Se il giocatore non ha attivato la torretta e non ha i soldi
+            else
+                SetText(false, Color.red, "Raccogli " + turret.buildingCost.ToString() + "$");
+
+            enabled = true;
+        }
+
+        private void SetText(bool canActiveKeyIcon, Color textColor, string textToShow)
+        {
+            image.enabled = canActiveKeyIcon;
+            text.color = textColor;
+            text.text = textToShow;
+        }
+        private void EnableTimer()
+        {
+            if (pickup.pickupType is Pickup.PickupType.Health)
+            {
+                enabled = true;
+                StartCoroutine(nameof(WaitForRespawning), pickup.timeToRespawn);
+            }
+        }
+
+        IEnumerator WaitForRespawning(float waitTime)
+        {
+            float elapsedTime = waitTime;
+            while (elapsedTime > 0)
+            {
+                elapsedTime -= Time.deltaTime;
+                text.text = ((int)elapsedTime).ToString(); //Mostra UI in base a quanto tempo manca per respawnare il pickup 
+                image.fillAmount = elapsedTime / waitTime;
+                yield return new WaitForEndOfFrame();
+            }
+            enabled = false;
+        }
+    }
+}
