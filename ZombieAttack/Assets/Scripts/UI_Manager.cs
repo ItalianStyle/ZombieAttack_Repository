072fa7a0754eb39a40e00a2d7911f0d2768cf8a7
ItalianStyle <60391359+ItalianStyle@users.noreply.github.com>
2021-08-04 22:56:53 +0200

    Update UI_Manager.cs
    - Created reference for PoisoningIcon Animator
    
    - Added event logic for starting PoisoningEffectIcon animation
    
    - Created SetPoisoningIcon_animation() to start all animations of PoisoningEffectIcon
    
    - Improved code visualization (moved all possible commented code to the bottom

--- ZombieAttack/Assets/Scripts/UI_Manager.cs
+++ ZombieAttack/Assets/Scripts/UI_Manager.cs
@@ -9,11 +9,6 @@ namespace ZombieAttack
 {
     public class UI_Manager : MonoBehaviour
     {
-        /*
-        public enum MenuType { MainMenu, Settings, Credits }
-
-        Animator scrollingCredits = null;
-        */
         [Header("Panels")]
         [SerializeField] CanvasGroup finishScreen = null;
         [SerializeField] CanvasGroup playerPanel = null;
@@ -23,64 +18,23 @@ namespace ZombieAttack
         [Space]
         [SerializeField] Text waveText = null;
         [SerializeField] Animator waveTextAnimator = null;
-
         [SerializeField] Animator poisonIconAnimator = null;
 
         [SerializeField] Text pickupTimerText;
         [SerializeField] Text timerText = null;
         [SerializeField] Text moneyText = null;
 
-        /*
-[SerializeField] CanvasGroup wavePanel = null;
-[SerializeField] CanvasGroup tutorialPanel = null;
-*/
-
-        /*[SerializeField] CanvasGroup settingsPanel = null;
-        [SerializeField] CanvasGroup creditsPanel = null;
-        //[SerializeField] CanvasGroup[] enemiesHPBars = null; 
-        */
         [Header("End screen stats")]
         [SerializeField] FinishScreen[] finishScreens = null;
 
         [Header("Buttons")]
-        [SerializeField] GameObject playButton = null;
-
-        /*[SerializeField] Button settingsButton = null;
-[SerializeField] Button creditsButton = null;
-[SerializeField] Button returnButtonFromSettings = null;
-[SerializeField] Button returnButtonFromCredits = null;
-*/
+        [SerializeField] GameObject playButton = null;        
         [SerializeField] GameObject resumeButton = null;
-        /*
-        [Header("Settings Sliders")]
-        [SerializeField] Slider masterVolumeSlider = null;
-        [SerializeField] Slider backgroundVolumeSlider = null;
-        [SerializeField] Slider sfxVolumeSlider = null;
-        */
+        
         Text titleFinishScreen = null;
 
-
         Camera cam;
-        /*
-        Equipment playerEquipment = null;
-        SimpleHealthBar bossHPBar = null;
-        SimpleHealthBar playerHPBar = null;
-        
-        //Usato da ObjectThrower per aggiornare la ricarica dell'arma
-        public Image GetCurrentWeaponReloadIcon
-        {
-            get
-            {
-                if (playerEquipment.currentWeaponType is Equipment.WeaponType.Sword)
-                    return swordIcon.transform.GetChild(0).GetComponent<Image>();
-
-                else if (playerEquipment.currentWeaponType is Equipment.WeaponType.FinalSword)
-                    return finalSwordIcon.transform.GetChild(0).GetComponent<Image>();
-
-                else return null;
-            }
-        }
-        */
+       
         public static UI_Manager instance;
 
         private void OnEnable()
@@ -94,6 +48,12 @@ namespace ZombieAttack
             DontDestroyOnLoad(this);
 
             SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnDisable()
+        {
+            PoisoningEffect.OnPoisoningEffectStarted += (duration) => SetPoisoningIcon_animation(true);
+            PoisoningEffect.OnPoisoningEffectFinished += () => SetPoisoningIcon_animation(false);
         }
 
         private void OnSceneLoaded(Scene scene, LoadSceneMode arg1)
@@ -144,6 +104,9 @@ namespace ZombieAttack
                     UpdateMoneyText();
                     //SetCanvasGroup(tutorialPanel, true);
 
+                    PoisoningEffect.OnPoisoningEffectStarted += (duration) => SetPoisoningIcon_animation(true); 
+                    PoisoningEffect.OnPoisoningEffectFinished += () => SetPoisoningIcon_animation(false);
+
                     break;
 
                 default:
@@ -151,20 +114,7 @@ namespace ZombieAttack
                     break;
             }
         }
-        /*
-        private void UpdateVolumeSettings()
-        {
-            masterVolumeSlider.value = AudioListener.volume;
-            backgroundVolumeSlider.value = MyAudioManager.instance.backgroundVolume;
-            sfxVolumeSlider.value = MyAudioManager.instance.sfxVolume;
-            
-        }
 
-        private void SetScrollingCredits(bool canScroll)
-        {
-            scrollingCredits.SetBool("CanScrollCredits", canScroll);
-        }
-        */
         //Trova i riferimenti alla UI in ogni scena
         private void GetReferences(int sceneIndex)
         {
@@ -211,11 +161,10 @@ namespace ZombieAttack
                     waveText = playerPanel.transform.Find("WaveText").GetComponent<Text>();
                     waveTextAnimator = waveText.GetComponent<Animator>();
 
-                    poisonIconAnimator = playerPanel.transform.Find("PoisoningSymbol").GetComponent<Animator>();
+                    poisonIconAnimator = playerPanel.transform.Find("PoisoningIcon_Background").GetComponent<Animator>();
 
                     timerText = playerPanel.transform.Find("TimerText").GetComponent<Text>();
                     moneyText = playerPanel.transform.Find("MoneyText").GetComponent<Text>();
-
 
                     //Trova il riferimento al tutorial
                     //tutorialPanel = GameObject.FindGameObjectWithTag("TutorialPanel").GetComponent<CanvasGroup>();
@@ -277,46 +226,7 @@ namespace ZombieAttack
             canvasGroup.interactable = canShow;
             canvasGroup.blocksRaycasts = canShow;
         }
-        /*
-        public void UpdateEquipmentIcon(EquipmentIcon.IconType equipmentIcon, float currentValue, float maxValue)
-        {
-            switch (equipmentIcon)
-            {
-                case EquipmentIcon.IconType.Shield:
-                    shieldIcon.equipmentIcon.fillAmount = currentValue / maxValue;
-                    break;
-
-                case EquipmentIcon.IconType.Sword:
-                    swordIcon.equipmentIcon.fillAmount = currentValue / maxValue;
-                    break;
-
-                case EquipmentIcon.IconType.FinalSword:
-                    finalSwordIcon.equipmentIcon.fillAmount = currentValue / maxValue;
-                    break;
-
-                default:
-                    Debug.LogError("Tipo di icona non definito!");
-                    break;
-            }
-        }
-
-        public void UpdateHPBar(GameObject gameObject, float currentValue, float maxValue)
-        {
-            //Controlla a chi si deve aggiornare la barra degli HP
-            if (gameObject.CompareTag("Player"))
-                playerHPBar.UpdateBar(currentValue, maxValue);
-
-            else if (gameObject.CompareTag("Enemy"))
-            {
-                //Se il nemico è il boss aggiorna la barra degli HP, altrimenti non fare niente
-                if (gameObject.GetComponent<Enemy>().enemyType is EnemyType.Boss)
-                    bossHPBar.UpdateBar(currentValue, maxValue);
-            }
-
-            else
-                Debug.LogError("Barra HP non riconosciuta");
-        }
-        */
+        
         public void SetHPBar(GameManager.EntityType characterType, bool canActive)
         {
             //Controlla quale barra degli HP deve essere attivata
@@ -363,14 +273,6 @@ namespace ZombieAttack
             SetHPBar(GameManager.EntityType.FinalObjective, canShow);
         }
 
-        /*
-        //Faccio cambiare la posizione della camera in base al menu attivo
-        public void SetCamera(MenuType menuType)
-        {
-            cameraPlacer.SetPosition((int)menuType);
-        }
-        */
-
         //Se non si passa alcun parametro, si setta il testo per la vittoria
         public void PlayWaveText(bool isVictoryText)
         {
@@ -385,11 +287,121 @@ namespace ZombieAttack
                 Wallet.InitializeWalletInstance();                   
             moneyText.text = Wallet.instance.GetCurrentMoney().ToString() + " $";
         }
+
         public void SetTimerText(bool startOrStopTimerText) => timerText.GetComponent<Animator>().SetBool("CanPlayTimerText", startOrStopTimerText);
 
-        public void SetPoisoningIcon(bool canActive)
+        private void SetPoisoningIcon_animation(bool canActive)
         {
-            poisonIconAnimator.SetBool("PoisonIsActive", canActive);
+            poisonIconAnimator.SetBool("CanActivePoisoningIconBackground", canActive); //Background
+            poisonIconAnimator.transform.GetChild(0).GetComponent<Animator>().SetBool("CanActiveTimeBar", canActive); //Timebar
+            poisonIconAnimator.transform.GetChild(0).GetChild(0).GetComponent<Animator>().SetBool("PoisonIsActive", canActive); //PoisonIcon
         }
+        /*
+        public enum MenuType { MainMenu, Settings, Credits }
+        Animator scrollingCredits = null;
+        */
+        /*
+        [Header("Settings Sliders")]
+        [SerializeField] Slider masterVolumeSlider = null;
+        [SerializeField] Slider backgroundVolumeSlider = null;
+        [SerializeField] Slider sfxVolumeSlider = null;
+        */
+        /*[SerializeField] Button settingsButton = null;
+        [SerializeField] Button creditsButton = null;
+        [SerializeField] Button returnButtonFromSettings = null;
+        [SerializeField] Button returnButtonFromCredits = null;
+        */
+        /*
+        [SerializeField] CanvasGroup wavePanel = null;
+        [SerializeField] CanvasGroup tutorialPanel = null;
+
+
+        /*[SerializeField] CanvasGroup settingsPanel = null;
+        [SerializeField] CanvasGroup creditsPanel = null;
+        //[SerializeField] CanvasGroup[] enemiesHPBars = null; 
+        */
+        /*
+       Equipment playerEquipment = null;
+       SimpleHealthBar bossHPBar = null;
+       SimpleHealthBar playerHPBar = null;
+
+       //Usato da ObjectThrower per aggiornare la ricarica dell'arma
+       public Image GetCurrentWeaponReloadIcon
+       {
+           get
+           {
+               if (playerEquipment.currentWeaponType is Equipment.WeaponType.Sword)
+                   return swordIcon.transform.GetChild(0).GetComponent<Image>();
+
+               else if (playerEquipment.currentWeaponType is Equipment.WeaponType.FinalSword)
+                   return finalSwordIcon.transform.GetChild(0).GetComponent<Image>();
+
+               else return null;
+           }
+       }
+       */
+        /*
+        private void UpdateVolumeSettings()
+        {
+           masterVolumeSlider.value = AudioListener.volume;
+           backgroundVolumeSlider.value = MyAudioManager.instance.backgroundVolume;
+           sfxVolumeSlider.value = MyAudioManager.instance.sfxVolume;
+
+        }
+
+        private void SetScrollingCredits(bool canScroll)
+        {
+           scrollingCredits.SetBool("CanScrollCredits", canScroll);
+        }
+        */
+
+        /*
+        //Faccio cambiare la posizione della camera in base al menu attivo
+        public void SetCamera(MenuType menuType)
+        {
+            cameraPlacer.SetPosition((int)menuType);
+        }
+        */
+
+        /*
+        public void UpdateEquipmentIcon(EquipmentIcon.IconType equipmentIcon, float currentValue, float maxValue)
+        {
+            switch (equipmentIcon)
+            {
+                case EquipmentIcon.IconType.Shield:
+                    shieldIcon.equipmentIcon.fillAmount = currentValue / maxValue;
+                    break;
+
+                case EquipmentIcon.IconType.Sword:
+                    swordIcon.equipmentIcon.fillAmount = currentValue / maxValue;
+                    break;
+
+                case EquipmentIcon.IconType.FinalSword:
+                    finalSwordIcon.equipmentIcon.fillAmount = currentValue / maxValue;
+                    break;
+
+                default:
+                    Debug.LogError("Tipo di icona non definito!");
+                    break;
+            }
+        }
+
+        public void UpdateHPBar(GameObject gameObject, float currentValue, float maxValue)
+        {
+            //Controlla a chi si deve aggiornare la barra degli HP
+            if (gameObject.CompareTag("Player"))
+                playerHPBar.UpdateBar(currentValue, maxValue);
+
+            else if (gameObject.CompareTag("Enemy"))
+            {
+                //Se il nemico è il boss aggiorna la barra degli HP, altrimenti non fare niente
+                if (gameObject.GetComponent<Enemy>().enemyType is EnemyType.Boss)
+                    bossHPBar.UpdateBar(currentValue, maxValue);
+            }
+
+            else
+                Debug.LogError("Barra HP non riconosciuta");
+        }
+        */
     }
 }
