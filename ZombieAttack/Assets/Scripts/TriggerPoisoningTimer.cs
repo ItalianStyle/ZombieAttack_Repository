ec7e970ed507e297e6de5beaf36f5f4880352d8f
ItalianStyle <60391359+ItalianStyle@users.noreply.github.com>
2021-08-04 20:24:53 +0200

    Created TriggerPoisoningTimer component
    It's responsible to decrease timeBar over time according to Poisoning effect duration

---
+++ ZombieAttack/Assets/Scripts/TriggerPoisoningTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ZombieAttack
+{
+    [RequireComponent(typeof(Image))]
+    public class TriggerPoisoningTimer : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Awake()
+        {
+            PoisoningEffect.OnPoisoningEffectStarted += (duration) => StartCoroutine(nameof(UpdatePoisoningTimeBar), duration);           
+            PoisoningEffect.OnPoisoningEffectFinished += () => StopCoroutine(nameof(UpdatePoisoningTimeBar)); ;
+        }
+
+        IEnumerator UpdatePoisoningTimeBar(float maxDuration)
+        {
+            Image timeBar = GetComponent<Image>();
+            float timeElapsed = maxDuration;
+            while (timeElapsed >= 0f)
+            {
+                timeElapsed -= Time.deltaTime;
+                timeBar.fillAmount = timeElapsed / maxDuration;
+                yield return null;
+            }           
+        }
+    }
+}
