033dcdb14c59141a52b0272021da5df9efbdff5c
ItalianStyle <60391359+ItalianStyle@users.noreply.github.com>
2021-08-09 19:09:50 +0200

    Improved Wallet code logic with events
    - Created OnCurrentMoneyChanged event
    - Created CurrentMoney property to set properly _currentMoney variable and call automatically OnCurrentMoneyChanged
    - Updated all component code according to CurrentMoney property
    
    - Introducing Wallet() constructor to eventually save the money or initialize them when restarting the game
    
    New methods:
    - LoadCheckpointMoney() to set currentMoney when restarting a wave that is not the first or the last one
    - SaveCurrentMoney() to save how many money the player has when he wins a wave

--- ZombieAttack/Assets/Scripts/Wallet.cs
+++ ZombieAttack/Assets/Scripts/Wallet.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 
 namespace ZombieAttack
@@ -6,32 +7,59 @@ namespace ZombieAttack
     public class Wallet
     {
         public static Wallet instance;
+        public static event Action OnCurrentMoneyChanged = delegate { };
 
-        int currentMoney = 0;
+        int _currentMoney = 0;
+        int checkpointCurrentMoney = 0;
+
+        public int CurrentMoney
+        {
+            get => _currentMoney;
+
+            set
+            {
+                _currentMoney = value;
+                if (_currentMoney < 0)
+                    _currentMoney = 0;
+
+                OnCurrentMoneyChanged.Invoke();
+            }
+        }
 
         public static void InitializeWalletInstance(int startValue = 0)
         {
             if(instance is null)          
                 instance = new Wallet();
-            instance.currentMoney = startValue;
+            instance.CurrentMoney = startValue;
         }
 
-        public int GetCurrentMoney() => currentMoney;
-             
-        public bool HasEnoughMoneyFor(int cost) => currentMoney >= cost;
+        Wallet()
+        {
+            CurrentMoney = checkpointCurrentMoney = 0;
 
-        public void ResetWallet() => currentMoney = 0;
+            EnemyManager.OnWaveKilled += SaveCurrentMoney;
+            EnemyManager.OnAllWavesKilled += () => InitializeWalletInstance();
+
+            GameManager.GameRestarted += LoadCheckpointMoney;
+        }
+
+        private void LoadCheckpointMoney(int waveIndex) => CurrentMoney = checkpointCurrentMoney;
+
+        private void SaveCurrentMoney() => checkpointCurrentMoney = CurrentMoney;
+
+        public int GetCurrentMoney() => CurrentMoney;
+             
+        public bool HasEnoughMoneyFor(int cost) => CurrentMoney >= cost;
+
+        public void ResetWallet() => CurrentMoney = 0;
 
         //Aggiorna il conto del giocatore, se true aggiunge i soldi, altrimenti li toglie
         public void UpdateCurrentMoney(int amount, bool isReward)
         {
-            if (isReward) currentMoney += amount;
+            if (isReward) 
+                CurrentMoney += amount;
             else
-            {
-                currentMoney -= amount;
-                if (currentMoney < 0)
-                    currentMoney = 0;
-            }
+                CurrentMoney -= amount;
         }
     }
 }
