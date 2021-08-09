553f1631c55ae864bc7d0bc068095aedfe7739e4
ItalianStyle <60391359+ItalianStyle@users.noreply.github.com>
2021-08-09 18:47:45 +0200

    Update EnemyManager.cs
    - Using events to detect when player wins a wave or the entire game
    - Created CurrentWave property to adjust currentWave value according to waves[] index
    - Created 2 more properties (IsLastWave and IsFirstWave) to check CurrentWave
    - Removed playerTransform reference
    - Created activeEnemies list to track all current active enemies in the game
    
    New methods:
    - OnSceneLoaded() to  initialize enemies, data structs and subscribe to necessary events
    - GetWavesFromAssets() to initialize waves[] with project wave files
    - RestartFromWave(int) to reset enemyManager data structs and start the wave text animation of the corresponding wave we want to start from
    
    - Improved SpawnWave() according to CurrentWave property
    - SpawnEnemy() is private and improved with CurrentWave property, Debugs and comments
    - Improved IncreaseKillCount() according to activeEnemies list, events, CurrentWave and IsLastWave properties

--- ZombieAttack/Assets/Scripts/EnemyManager.cs
+++ ZombieAttack/Assets/Scripts/EnemyManager.cs
@@ -1,63 +1,148 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
 using Random = UnityEngine.Random;
 
 namespace ZombieAttack
 {
     public class EnemyManager : MonoBehaviour
     {
-        [SerializeField] List<Transform> spawnPoints = null;
-        Transform finalObjectiveTransform = null;
-        Transform playerTransform;
-        [SerializeField] Wave[] waves = null;
-        public int currentWave = 0;
-        int killedEnemies = 0;       
-        int[] spawnedEnemies;
+        public static event Action OnWaveKilled = delegate { };
+        public static event Action OnAllWavesKilled = delegate { };
+
+        public int CurrentWave
+        {
+            get => _currentWave;
+
+            private set
+            {
+                _currentWave = value;
+                if (_currentWave >= waves.Length)
+                    _currentWave = waves.Length - 1;
+                else if (_currentWave < 0)
+                    _currentWave = 0;
+            }
+        }
+        public bool IsLastWave => CurrentWave == waves.Length - 1;
+        public bool IsFirstWave => CurrentWave == 0;
 
         public static EnemyManager instance;
 
-        private void Awake()
+        [SerializeField] List<Transform> spawnPoints = null;
+        [SerializeField] Wave[] waves = null;
+
+        Transform finalObjectiveTransform = null;
+        int _currentWave = 0;
+        int killedEnemies = 0;       
+        int[] spawnedEnemies;
+        List<GameObject> activeEnemies = new List<GameObject>();
+
+        private void OnEnable()
         {
-            instance = this;
-            finalObjectiveTransform = GameObject.FindGameObjectWithTag("Finish").transform;
+            if (instance != null && instance != this)
+                DestroyImmediate(gameObject);
+            else
+            {
+                instance = this;
+                SceneManager.sceneLoaded += OnSceneLoaded;
+            }              
+        }      
+
+        private void OnDisable()
+        {
+            Timer.OnPreparationTimerFinished -= SpawnWave;
+            WaveBeginnerFlag.OnStartEnemyWave -= (timeToEndWave) => SpawnWave();
+            CancelInvoke(nameof(SpawnEnemy));
         }
 
-        private void Start()
+        private void OnSceneLoaded(Scene scene, LoadSceneMode arg1)
         {
-            for(int i = 0; i < transform.childCount; i++)
-                spawnPoints.Add(transform.GetChild(i));
+            if(scene.buildIndex == 1)
+            { 
+                finalObjectiveTransform = GameObject.FindGameObjectWithTag("Finish").transform;
+                //Initialize Spawn points
+                for (int i = 0; i < transform.childCount; i++)
+                    spawnPoints.Add(transform.GetChild(i));
+
+                CurrentWave = 0;
+                GetWavesFromAssets();
+                foreach (Wave wave in waves)
+                    wave.InitializeEnemyTypesIndexList();
+
+                spawnedEnemies = new int[waves[CurrentWave].maxEnemyTypes.Length];
+                activeEnemies = new List<GameObject>();
+
+                Timer.OnPreparationTimerFinished += () =>
+                {
+                    CurrentWave++;
+                    UI_Manager.instance.PlayWaveTextAnimation(false);
+                };
+
+                WaveBeginnerFlag.OnStartEnemyWave += (timeToEndWave) => SpawnWave();
+
+                GameManager.GameRestarted += (waveIndex) => RestartFromWave(waveIndex);
+            }
+        }
+
+        //Looks for wave assets
+        private void GetWavesFromAssets()
+        {
+            string[] guids;
+
+            // search for a ScriptObject called ScriptObj
+            guids = AssetDatabase.FindAssets("t:Wave", new[] { "Assets/Scriptable_objects/Enemy_Waves" });
             
-            currentWave = 0;
+            for(int i = 0; i < guids.Length; i++)
+            {
+                string wavePath = AssetDatabase.GUIDToAssetPath(guids[i]);
+                if (wavePath.Contains("wave_" + (i+1).ToString()))
+                    waves[i] = (Wave)AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(guids[i]), typeof(Wave));
+            }
+        }
+
+        private void RestartFromWave(int waveIndex)
+        {
+            GameManager.PrintExecutionLocation(this);
+            foreach (GameObject enemy in activeEnemies)
+                enemy.SetActive(false);
+
+            GetWavesFromAssets();
             foreach (Wave wave in waves)
                 wave.InitializeEnemyTypesIndexList();
-            spawnedEnemies = new int[waves[0].maxEnemyTypes.Length];
-            playerTransform = GameManager.instance.player.transform;
+
+            CurrentWave = waveIndex;
+            spawnedEnemies = new int[waves[CurrentWave].maxEnemyTypes.Length];
+            activeEnemies = new List<GameObject>();
+            
+            //Start the wave
+            UI_Manager.instance.PlayWaveTextAnimation(isVictoryText: false);
+
+
         }
 
         public void SpawnWave()
         {
+            GameManager.PrintExecutionLocation(this);
             if (waves.Length > 0)
-            {
-                if (currentWave >= 0 && currentWave < waves.Length)
-                {
-                    //Start timer
-                    spawnedEnemies = new int[waves[currentWave].maxEnemyTypes.Length];
-                    InvokeRepeating(nameof(SpawnEnemy), 0f, waves[currentWave].timeBetweenSpawns);
-                }
+            {              
+                spawnedEnemies = new int[waves[CurrentWave].maxEnemyTypes.Length];
+                InvokeRepeating(nameof(SpawnEnemy), 0f, waves[CurrentWave].timeBetweenSpawns);
             }
         }
 
-        public void SpawnEnemy()
+        void SpawnEnemy()
         {
+            GameManager.PrintExecutionLocation(this);
             //Choose enemy type to spawn
-            int enemyTypeIndex = waves[currentWave].SelectEnemyType();
+            int enemyTypeIndex = waves[CurrentWave].SelectEnemyType();
 
             //If the list of callable type of enemies is not empty
             if (enemyTypeIndex != -1)
             {
                 //If spawned enemies haven't reached the maximum number allowed
-                if (spawnedEnemies[enemyTypeIndex] < waves[currentWave].maxEnemyTypes[enemyTypeIndex])
+                if (spawnedEnemies[enemyTypeIndex] < waves[CurrentWave].maxEnemyTypes[enemyTypeIndex])
                 {
                     GameObject enemy; 
                     switch(enemyTypeIndex)
@@ -80,19 +165,25 @@ namespace ZombieAttack
                             break;
                     }
                     SetupEnemy(enemy);
+                    Debug.Log("Nemico è attivo");
                     enemy.SetActive(true);
+                    Debug.Log("Aggiungo il nemico alla lista dei nemici attivi");
+                    activeEnemies.Add(enemy);
                     //Increase enemy counting for this type
                     spawnedEnemies[enemyTypeIndex]++;
                 }
-                //else remove the index from the list and restart the method
+                //else if the list of callable type of enemies is empty, remove the index from the list and restart the method
                 else
                 {
-                    waves[currentWave].DiscardEnemyType(enemyTypeIndex);
+                    waves[CurrentWave].DiscardEnemyType(enemyTypeIndex);
                     SpawnEnemy();
                 }
             }
-            else          
+            else
+            {
+                Debug.LogWarning("List of callable type of enemies is empty");
                 CancelInvoke(nameof(SpawnEnemy));
+            }              
         }
 
         private void SetupEnemy(GameObject enemy)
@@ -100,32 +191,39 @@ namespace ZombieAttack
             enemy.GetComponent<Rigidbody>().velocity = Vector3.zero;
             //Choose spawnpoint
             enemy.transform.position = spawnPoints[Random.Range(0, spawnPoints.Count)].position;
-            enemy.GetComponent<EnemyMovement>().SetDestination(enemy.CompareTag("EnemyMedium")? playerTransform : finalObjectiveTransform);          
+            enemy.GetComponent<EnemyMovement>().SetDestination(enemy.CompareTag("EnemyMedium")? GameManager.instance.player.transform : finalObjectiveTransform);          
             enemy.GetComponent<Health>().OnEnemyDead += IncreaseKillCount;
         }
 
         private void IncreaseKillCount(Health enemyHealth)
         {
-            killedEnemies++;
-            enemyHealth.OnEnemyDead -= IncreaseKillCount;
-            //If wave is finished
-            if (killedEnemies >= waves[currentWave].MaxEnemies)
+            GameManager.PrintExecutionLocation(this);
+            if (activeEnemies.Contains(enemyHealth.gameObject))
             {
-                killedEnemies = 0;
-                if (currentWave >= waves.Length -1)
+                activeEnemies.Remove(enemyHealth.gameObject);
+                killedEnemies++;
+                enemyHealth.OnEnemyDead -= IncreaseKillCount;
+
+                //If wave is finished
+                if (killedEnemies >= waves[CurrentWave].MaxEnemies)
                 {
-                    currentWave = 0;
-                    //Vittoria del gioco
-                    GameManager.instance.SetStatusGame(GameManager.GameState.Won);
-                    UI_Manager.instance.SetFinishScreen(GameManager.GameState.Won);
+                    killedEnemies = 0;
+                    if (IsLastWave)
+                    {
+                        CurrentWave = 0;
+                        //Vittoria del gioco
+                        OnAllWavesKilled.Invoke();
+                    }
+                    else
+                    {
+                        //Vittoria dell'ondata
+                        OnWaveKilled.Invoke();
+                    }
                 }
-                else
-                {
-                    currentWave++;
-                    //Vittoria dell'ondata
-                    GameManager.instance.SetStatusGame(GameManager.GameState.WaveWon);
-                    UI_Manager.instance.SetFinishScreen(GameManager.GameState.WaveWon);
-                }
+            }
+            else
+            {
+                Debug.LogError("Il nemico non cè nella lista dei nemici attivi, non si può rimuovere");
             }
         }
     }
