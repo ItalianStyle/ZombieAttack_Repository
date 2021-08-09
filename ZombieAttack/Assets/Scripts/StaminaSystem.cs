5dd6c013625f0271848071c8541e32e61ce76c6f
ItalianStyle <60391359+ItalianStyle@users.noreply.github.com>
2021-08-09 13:20:36 +0200

    Improved Stamina logic
    - Introducing canRecoverStamina bool variable to check if player can pick up the Stamina pickup
    
    - Created Recover() method to recover some stamina

--- ZombieAttack/Assets/Scripts/StaminaSystem.cs
+++ ZombieAttack/Assets/Scripts/StaminaSystem.cs
@@ -14,6 +14,7 @@ public class StaminaSystem : MonoBehaviour
     [SerializeField] SimpleHealthBar staminaBar;
     public bool isPlayerRunning = false;
     float _currentStamina;
+    public bool canRecoverStamina = false;
 
     public float CurrentStamina
     {
@@ -27,14 +28,18 @@ public class StaminaSystem : MonoBehaviour
                 _currentStamina = 0;
                 OnStaminaEmpty.Invoke();
             }
-            else if (_currentStamina > maxStamina)
+            else if (_currentStamina >= maxStamina)
             {
                 _currentStamina = maxStamina;
+                canRecoverStamina = false;
                 OnStaminaFull.Invoke();
             }
+            else
+                canRecoverStamina = true;
             staminaBar.UpdateBar(_currentStamina, maxStamina);
         }
     }
+
     private void Start() => CurrentStamina = maxStamina;
 
     private void OnEnable()
@@ -51,5 +56,10 @@ public class StaminaSystem : MonoBehaviour
             CurrentStamina -= Time.deltaTime * usedStaminaFactor;
         else if (CurrentStamina < maxStamina)
             CurrentStamina += Time.deltaTime * recoverStaminaFactor;
-    }  
+    }
+    
+    public void Recover(float amount)
+    {
+        CurrentStamina += amount;
+    }
 }
