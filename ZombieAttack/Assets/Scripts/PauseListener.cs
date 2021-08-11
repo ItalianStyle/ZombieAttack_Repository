09e9f34edeb284eadfa1e78a606b2f6fa13738f3
ItalianStyle <60391359+ItalianStyle@users.noreply.github.com>
2021-08-11 17:42:59 +0200

    Added listeners to GameRestarted event in PauseListener

--- ZombieAttack/Assets/Scripts/PauseListener.cs
+++ ZombieAttack/Assets/Scripts/PauseListener.cs
@@ -8,7 +8,11 @@ namespace ZombieAttack
         public static event Action OnPauseKeyPressed = delegate { };
 
         //Im in playing the game state, don't listen to GameResumed event anymore
-        private void OnEnable() => GameManager.GameResumed -= () => enabled = true;
+        private void OnEnable()
+        {
+            GameManager.GameResumed -= () => enabled = true;
+            GameManager.GameRestarted += (_) => enabled = true;
+        }
 
         // Update is called once per frame
         void Update()
@@ -21,6 +25,10 @@ namespace ZombieAttack
         }
 
         //Im in pause state, return to listen pause key when game is resumed
-        private void OnDisable() => GameManager.GameResumed += () => enabled = true;
+        private void OnDisable()
+        {
+            GameManager.GameResumed += () => enabled = true;
+            GameManager.GameRestarted += (_) => enabled = true;
+        }
     }
 }
