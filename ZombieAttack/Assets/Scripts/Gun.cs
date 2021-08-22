72df3f6b79ee6aad516b658ab9d40a36677038fa
ItalianStyle <lorenzo.petraroli@hotmail.it>
2021-08-22 09:16:54 +0200

    Improved Gun component with VFX and SFX

--- ZombieAttack/Assets/Scripts/Gun.cs
+++ ZombieAttack/Assets/Scripts/Gun.cs
@@ -4,42 +4,59 @@ using UnityEngine;
 
 namespace ZombieAttack
 {
+    [RequireComponent(typeof(AudioSource))]
     public class Gun : MonoBehaviour
     {
         public enum GunType { Rifle, Shotgun }
         public GunType gunType;
 
         [SerializeField] MeshRenderer[] gunVisual;
+        [SerializeField] AudioClip gunShootSFX;
+        [SerializeField] AudioClip gunLoadSFX;
         Transform muzzleTransform;
         BulletHandler bulletParticles;
 
-        Transform playerTransform;
-        
+        PlayerMovement playerMovement;
+        AudioSource gunAudioSource = null;
+
         [Header("Stats")]
         [SerializeField] float reloadTime = .2f;
         public float damage = 1f;
         public int cost;
         bool canShoot = true;
+        bool canPlaySound = false;
 
         private void Awake()
         {
             gunVisual = GetComponentsInChildren<MeshRenderer>();
             muzzleTransform = transform.Find("Muzzle");
             bulletParticles = muzzleTransform.GetComponentInChildren<BulletHandler>();
-            playerTransform = transform.parent;
+            playerMovement = transform.parent.GetComponent<PlayerMovement>();
+            gunAudioSource = GetComponent<AudioSource>();
         }
 
-        private void Start()
+        private void Start() => bulletParticles.BulletDamage = damage;
+
+        private void OnEnable()
         {
-            bulletParticles.BulletDamage = damage;
+            MyAudioManager.OnInstanceReady += () => canPlaySound = true;
+            if(canPlaySound)
+                MyAudioManager.instance.PlayAudioSourceWithClip(gunAudioSource, gunLoadSFX, true);
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonUp(0))
+                bulletParticles.SetShootingEffect(false);
         }
 
         public void Shoot(SimpleHealthBar gunBar)
         {
             if (canShoot)
             {
-                playerTransform.GetComponent<PlayerMovement>().FaceCamera();
-                bulletParticles.Particles.Play();
+                playerMovement.FaceCamera();
+                MyAudioManager.instance.PlayAudioSourceWithClip(gunAudioSource, gunShootSFX, true);
+                bulletParticles.SetShootingEffect(true);
                 //Post-shoot stuff
                 StartCoroutine(nameof(Reload), gunBar);
                 canShoot = false;
@@ -52,7 +69,8 @@ namespace ZombieAttack
             {
                 for (int i = 0; i < gunVisual.Length; i++)
                     gunVisual[i].enabled = isActive;
-               
+
+                bulletParticles.SetShootingEffect(false);
                 enabled = isActive;
             }
         }
