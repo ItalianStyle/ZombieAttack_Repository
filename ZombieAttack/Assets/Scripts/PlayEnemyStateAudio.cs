dca87f3e357a716e36d2f0517e7640715a280766
ItalianStyle <lorenzo.petraroli@hotmail.it>
2021-08-22 08:22:07 +0200

    Created Play__StateAudio components to play audio properly according to object's state

---
+++ ZombieAttack/Assets/Scripts/PlayEnemyStateAudio.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace ZombieAttack
+{
+    public class PlayEnemyStateAudio : StateMachineBehaviour
+    {
+        float timeElapsed = 0f;
+        AudioSource enemyAudioSource;
+        EnemyAudioPlayer enemyAudioPlayer;
+
+        // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            enemyAudioSource = animator.GetComponent<AudioSource>();
+            enemyAudioPlayer = animator.GetComponent<EnemyAudioPlayer>();
+            timeElapsed = 0f;
+        }
+
+        // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+        override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            timeElapsed += Time.deltaTime;
+            if (stateInfo.IsName("Walking"))
+            {
+                if (!enemyAudioSource.isPlaying && timeElapsed > enemyAudioPlayer.GetBreathSFX.length && !GameManager.instance.isPaused)
+                {
+                    MyAudioManager.instance.PlayAudioSourceWithClip(audioSource: enemyAudioSource, enemyAudioPlayer.GetBreathSFX, false);
+                    timeElapsed = 0f;
+                }
+            }
+            else if(stateInfo.IsName("Attacking"))
+            {
+                if (!enemyAudioSource.isPlaying && timeElapsed > enemyAudioPlayer.GetAttackSFX.length && !GameManager.instance.isPaused)
+                {
+                    MyAudioManager.instance.PlayAudioSourceWithClip(audioSource: enemyAudioSource, enemyAudioPlayer.GetAttackSFX, false);
+                    timeElapsed = 0f;
+                }
+            }
+        }
+    }
+}
---
+++ ZombieAttack/Assets/Scripts/PlayTurretStateAudio.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace ZombieAttack
+{
+    public class PlayTurretStateAudio : StateMachineBehaviour
+    {
+        readonly string[] turretActiveStateNames = { "StartShooting", "Shooting", "EndShooting" };
+        [SerializeField] AudioClip[] turretActive_SFXs = new AudioClip[3];
+        
+        AudioSource turretAudioSource = null;
+        
+        // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if(!turretAudioSource)
+                turretAudioSource = animator.GetComponent<AudioSource>();
+
+            for(int i = 0; i < turretActiveStateNames.Length; i++)
+            {
+                if (stateInfo.IsName(turretActiveStateNames[i]))
+                {
+                    MyAudioManager.instance.PlayAudioSourceWithClip(turretAudioSource, turretActive_SFXs[i], false);
+                    break;
+                }
+            }     
+        }
+    }
+}
